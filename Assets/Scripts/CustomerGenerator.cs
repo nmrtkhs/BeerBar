a67b1f53210d6ec8a9aeecd258bdd2334ea168db
Masami1221 <masami.arai1233@gmail.com>
2021-01-25 22:10:47 +0900

    コミットのやり直し

---
+++ Assets/Scripts/CustomerGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class CustomerGenerator : MonoBehaviour
+{
+    public GameObject[] customers;
+    float span = 3.0f;
+    float delta = 0;
+    GameObject[] realCustomers;
+    void Start()
+    {
+      realCustomers = new GameObject[3];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+      this.delta += Time.deltaTime;
+      if (this.delta > this.span)
+      {
+        // 客がテーブルにいるかどうかをチェック
+        List<int> checkCustomers = new List<int>();
+        for (var i = 0; i < realCustomers.Length; ++i) {
+          if (realCustomers[i] == null) {
+            checkCustomers.Add(i);
+          }
+        }
+        // いないテーブルに対して客を生成する
+        if (checkCustomers.Any()) {
+          int tableNo = checkCustomers[ Random.Range(0, checkCustomers.Count) ];
+          realCustomers[tableNo] = Instantiate(customers[tableNo]) as GameObject;
+          this.delta = 0;
+          realCustomers[tableNo].transform.position = new Vector3(tableNo * 5.0f - 6.5f, -3.4f, 0);
+        }
+      }
+    }
+}
---
+++ Assets/Scripts/glassScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glassScript : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        this.screenPoint = Camera.main.WorldToScreenPoint(transform.position); //オブジェクトのポジションからスクリーン座標を取りたい時に使用する
+        this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z)); 
+        //ドラッグする時にオブジェクトがカーソルに自動でセンタリングしないようにする。マウスと位置とオブジェクトの基準点の差をoffsetに代入し、ドラッグが始まったらオフセットする。
+    }
+  
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z); //ドラッグ時のマウス位置をスクリーン座標に変換する
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;　// //上記にクリックした場所の差を足すことによって、オブジェクトを移動する座標位置を求める
+        transform.position = currentPosition; //オブジェクトの位置を変更する
+    }
+}
